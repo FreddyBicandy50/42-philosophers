e8eb0f2e1ec2a0e39fe7d84c6df30e84fbc13628
Frederic Bicandy <fbicandy@c2r5s4.42beirut.com>
2025-06-11 21:36:48 +0300

    doing

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,38 +6,35 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 18:56:01 by fbicandy          #+#    #+#             */
-/*   Updated: 2025/06/11 21:34:38 by fbicandy         ###   ########.fr       */
+/*   Updated: 2025/06/11 21:36:34 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	precise_usleep(long usec, t_table *table)
-{
-	time_t	start;
-	time_t	elapsed;
-	time_t	rem;
+// void	precise_usleep(long usec, t_table *table)
+// {
+// 	time_t	start;
+// 	time_t	elapsed;
+// 	time_t	rem;
 
-	start = get_time_in_us();
-	while (get_time_in_us() - start < usec)
-	{
-		if (has_sim_ended(table))
-			break ;
-		elapsed = get_time_in_us();
-		rem = usec - elapsed;
-		if (rem > 1e3)
-			usleep(rem / 2);
-		else
-		{
-			while (get_time_in_us() - start < usec)
-				;
-		}
-	}
-}
+// 	start = get_time_in_us();
+// 	while (get_time_in_us() - start < usec)
+// 	{
+// 		if (has_sim_ended(table))
+// 			break ;
+// 		elapsed = get_time_in_us();
+// 		rem = usec - elapsed;
+// 		if (rem > 1e3)
+// 			usleep(rem / 2);
+// 		else
+// 		{
+// 			while (get_time_in_us() - start < usec)
+// 				;
+// 		}
+// 	}
+// }
 
-/*
- * we'll check if the sim has already ended
- */
 bool	has_sim_ended(t_table *table)
 {
 	pthread_mutex_lock(&table->table_mutex);
@@ -47,9 +44,6 @@ bool	has_sim_ended(t_table *table)
 		return (pthread_mutex_unlock(&table->table_mutex), false);
 }
 
-/*
- * we'll wait for all philos to be created before we start
- */
 void	wait_all_threads(t_table *table)
 {
 	while (1)
